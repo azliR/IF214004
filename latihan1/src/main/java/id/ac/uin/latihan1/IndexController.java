d382da0b6603dc7c813673c4ef8da1b2d2b927dc
azliR <rizalhadiyansah@gmail.com>
2022-03-09 16:18:24 +0700

    Update index controller

--- latihan1/src/main/java/id/ac/uin/latihan1/IndexController.java
+++ latihan1/src/main/java/id/ac/uin/latihan1/IndexController.java
@@ -1,14 +1,17 @@
 package id.ac.uin.latihan1;
 
+import java.time.LocalDateTime;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
 public class IndexController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello";
+    public String hello(@RequestParam String nama) {
+        return "Halo " + nama + "! Waktu hari ini " + LocalDateTime.now();
     }
 }
